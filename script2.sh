be9859a1d549e1ff39676ae558a6622188b2c963
jad <jadevideolab.ae>
2025-03-14 16:52:19 +0530

    sagar update

--- script2.sh
+++ script2.sh
@@ -2,4 +2,5 @@ ls
 i have created a branch its name is branch1
 branch2 first
 branch2 2nd commit added changes
-branch2 3rd commit added changes
+branch2 3rd commit added change
+hey this is sagar and these are my changes
